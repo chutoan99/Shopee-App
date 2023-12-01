c295a7855379d221faf720d6c58caf91b0b01ea2
TOAN\truon <truongvanchutoan1999@gmail.com>
2023-12-01 19:35:55 +0700

    first commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useCallback, useEffect } from "react";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import RoutesConfig from "./src/route";
 
 export default function App() {
+  const [fontsLoaded, fontError] = useFonts({
+    "Inter-Black": require("./src/assets/fonts/Inter-Black.ttf"),
+    "Inter-Bold": require("./src/assets/fonts/Inter-Bold.ttf"),
+    "Inter-ExtraBold": require("./src/assets/fonts/Inter-ExtraBold.ttf"),
+    "Inter-ExtraLight": require("./src/assets/fonts/Inter-ExtraLight.ttf"),
+    "Inter-Light": require("./src/assets/fonts/Inter-Light.ttf"),
+    "Inter-Medium": require("./src/assets/fonts/Inter-Medium.ttf"),
+    "Inter-Regular": require("./src/assets/fonts/Inter-Regular.ttf"),
+    "Inter-SemiBold": require("./src/assets/fonts/Inter-SemiBold.ttf"),
+    "Inter-Thin": require("./src/assets/fonts/Inter-Thin.ttf"),
+  });
+
+  const loadResourcesAndDataAsync = useCallback(async () => {
+    try {
+      await SplashScreen.preventAutoHideAsync();
+      if (fontsLoaded || fontError) {
+        await SplashScreen.hideAsync();
+      }
+    } catch (e) {
+      console.warn(e);
+    }
+  }, [fontsLoaded, fontError]);
+  useEffect(() => {
+    loadResourcesAndDataAsync();
+  }, [loadResourcesAndDataAsync]);
+
+  if (!fontsLoaded && !fontError) {
+    return null; 
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <RoutesConfig></RoutesConfig>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
